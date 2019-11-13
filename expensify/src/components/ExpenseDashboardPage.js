57c1075b02c1d0483c82900a888a3fd490491b4d
Oshalusi John <oshalusijohn@gmail.com>
2019-11-13 04:56:47 +0100

    Updates from React

--- expensify/src/components/ExpenseDashboardPage.js
+++ expensify/src/components/ExpenseDashboardPage.js
@@ -1,8 +1,10 @@
-import React from 'react';
-import ExpenseList from './ExpenseList';
+import React from "react";
+import ExpenseList from "./ExpenseList";
+import ExpenseListFilter from "./ExpenseListFilter";
 
 const ExpenseDashboardPage = () => (
   <div>
+    <ExpenseListFilter />
     <ExpenseList />
   </div>
 );
--- expensify/src/components/ExpenseList.js
+++ expensify/src/components/ExpenseList.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import ExpenseListItem from './ExpenseListItem';
+import selectExpenses from '../selectors/expenses';
 
-const ExpenseList = props => {
-  return (
-    <div>
-      <h1> Expense List </h1>
-      {props.filters.text}
-      {props.expenses.length}
-    </div>
-  );
-};
+const ExpenseList = (props) => (
+  <div>
+    <h1>Expense List</h1>
+    {props.expenses.map((expense) => {
+      return <ExpenseListItem key={expense.id} {...expense} />;
+    })}
+  </div>
+);
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
-    expenses: state.expenses,
-    filters: state.filters
+    expenses: selectExpenses(state.expenses, state.filters)
   };
 };
 
